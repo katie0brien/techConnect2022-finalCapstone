295c55bde98c141f0eb6eaefc83610def024aaed
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2022-08-15 11:42:08 -0400

    changes

--- database/03-create-users.sql
+++ database/03-create-users.sql
@@ -2,7 +2,7 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER city_tours_owner WITH PASSWORD 'city_tours_owner1';
+CREATE USER brewery_finder_owner WITH PASSWORD 'city_tours_owner1';
 
 GRANT ALL 
 ON ALL TABLES IN SCHEMA public
@@ -12,7 +12,7 @@ GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
 TO city_tours_owner;
 
-CREATE USER city_tours_appuser WITH PASSWORD 'city_tours_appuser1';
+CREATE USER brewery_finder_appuser WITH PASSWORD 'city_tours_appuser1';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
