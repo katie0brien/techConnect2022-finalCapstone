89cf2a8110b45526285e9d70fbd6f1e9d4a0574f
Mohamed Fofana <mohamed.fofana63@yahoo.com>
2022-08-16 11:24:03 -0400

    created tables for landmark and relator table user_landmark

--- database/02-create-schema.sql
+++ database/02-create-schema.sql
@@ -5,7 +5,7 @@
 BEGIN;
 
 -- CREATE statements go here
-DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS app_user, landmark, user_landmark;
 
 CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
@@ -14,9 +14,28 @@ CREATE TABLE app_user (
   fname varchar(32) NOT NULL,
   lname varchar(32) NOT NULL,
   email varchar(64) NOT NULL,
-  phone_number vrachar(13) NOT NULL,
+  phone_number varchar(13) NOT NULL,
   role varchar(32),
   salt varchar(255) NOT NULL
+);
+
+CREATE TABLE landmark (
+  id varchar(50) PRIMARY KEY,
+  latitude varchar(25),
+  longitude varchar(25),
+  name varchar(50) NOT NULL UNIQUE,
+  street_address varchar(100) UNIQUE,
+  city varchar(25),
+  state_or_region varchar(50),
+  country varchar(50)
+);
+
+CREATE TABLE user_landmark (
+  user_id int,
+  landmark_id varchar(50),
+  FOREIGN KEY (user_id) REFERENCES app_user(id),
+  FOREIGN KEY (landmark_id) REFERENCES landmark(id),
+  PRIMARY KEY (user_id, landmark_id)
 );
 
 COMMIT;
