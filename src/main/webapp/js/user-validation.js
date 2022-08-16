9f0f27d5ee759baf7696026214186cb1d8a3abc3
Katherine OBrien <katien160@gmail.com>
2022-08-16 12:37:55 -0400

    changes made to newUser.jsp and user-validation.js

--- src/main/webapp/js/user-validation.js
+++ src/main/webapp/js/user-validation.js
@@ -2,11 +2,20 @@ $(document).ready(function () {
     $.validator.addMethod('capitals', function(thing){
         return thing.match(/[A-Z]/);
     });
+    $.validator.addMethod('pattern', function(thing2){
+        return thing2.match(/^\(\d{3}\)\d{3}-\d{4}/);
+    });
+
     $("form").validate({
 
         rules : {
             userName : {
                 required : true
+            },
+            phoneNumber : {
+                required: true,
+                minlength:  13,
+                pattern: true,
             },
             password : {
                 required : true,
@@ -22,6 +31,10 @@ $(document).ready(function () {
             password: {
                 minlength: "Password too short, make it at least 8 characters",
                 capitals: "Field must contain a capital letter",
+            },
+            phoneNumber:{
+                pattern: "Please enter a phone number in the format (###)###-####",
+                minlength: "Please enter a phone number in the format (###)###-####",
             },
             confirmPassword : {
                 equalTo : "Passwords do not match"
