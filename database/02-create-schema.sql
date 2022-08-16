6b8752892ba30caeb17ac58b9416a0b3e8c18391
Sarah Shaffer <shaffer.595@buckeyemail.osu.edu>
2022-08-16 09:39:47 -0400

    Updated back end for Modify User registrtaion Form story

--- database/02-create-schema.sql
+++ database/02-create-schema.sql
@@ -11,6 +11,10 @@ CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
   user_name varchar(32) NOT NULL UNIQUE,
   password varchar(32) NOT NULL,
+  fname varchar(32) NOT NULL,
+  lname varchar(32) NOT NULL,
+  email varchar(64) NOT NULL,
+  phone_number vrachar(13) NOT NULL,
   role varchar(32),
   salt varchar(255) NOT NULL
 );
