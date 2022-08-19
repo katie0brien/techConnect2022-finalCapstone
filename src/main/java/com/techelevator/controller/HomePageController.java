3770646c147e69ec36bec1b08120c61ab760798d
Katherine OBrien <katien160@gmail.com>
2022-08-19 12:37:40 -0400

    added login functionality

--- src/main/java/com/techelevator/controller/HomePageController.java
+++ src/main/java/com/techelevator/controller/HomePageController.java
@@ -1,15 +1,31 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(path="/")
 public class HomePageController {
 
+    private UserDAO userDao;
+
+    @Autowired
+    public HomePageController(UserDAO userDao){
+        this.userDao = userDao;
+    }
+
     @RequestMapping(path="/home", method= RequestMethod.GET)
-    public String displayHomePage(){ return "homePage";}
+    public String displayHomePage(@RequestParam String userName, @RequestParam String password)
+    {
+        if ( userDao.userNamePasswordExist(userName, password) ) {
+            return "homePage";
+        }
+        return "login";
+    }
 
 
 }
